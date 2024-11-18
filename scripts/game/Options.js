f4e4b19f8849db906894f5a20db2bd6e63d332c4
SherlockWalker <140088079+SherlockWalker@users.noreply.github.com>
2024-11-18 18:50:25 +0700

    OPTIONS JS HALLELUJAH

--- scripts/game/Options.js
+++ scripts/game/Options.js
@@ -26,7 +26,7 @@ Loader.addSounds([
 			HOW_MANY_PROMPTS = 1;
 		}
 		Game.CLICK_TO_ADVANCE = !Game.CLICK_TO_ADVANCE;
-		text_automatic_toggle.innerHTML = Game.CLICK_TO_ADVANCE ? "nhấn chuột" : "tự động";
+		text_automatic_toggle.innerHTML = Game.CLICK_TO_ADVANCE ? "nhấp chuột" : "tự động";
 
 		// Sound
 		sfx( Game.CLICK_TO_ADVANCE ? "ui_button2" : "ui_button1");
